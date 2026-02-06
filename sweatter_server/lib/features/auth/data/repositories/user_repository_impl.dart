cb4ac61b194c34de5741a4a73ebd96e00eba79b6
arampap117 <arampap117@gmail.com>
2026-02-07 02:04:55 +0300

    реализована функция createUser

--- sweatter_server/lib/features/auth/data/repositories/user_repository_impl.dart
+++ sweatter_server/lib/features/auth/data/repositories/user_repository_impl.dart
@@ -1,16 +1,23 @@
+import 'package:drift/drift.dart';
 import 'package:sweatter_server/features/auth/data/db/user_database.dart';
 import 'package:sweatter_server/features/auth/domain/repositories/i_user_repository.dart';
 
 final class UserRepositoryImpl implements IUserRepository {
-  const UserRepositoryImpl({required this.database});
-  final UserDatabase database;
+  const UserRepositoryImpl({required this.db});
+  final UserDatabase db;
 
   @override
   Future<void> createUser({
     required final String email,
     required final String name,
     required final String passwordHash,
-  }) async {
-    database.into().insert(entity);
-  }
+  }) async => await db
+      .into(db.userTable)
+      .insert(
+        UserTableCompanion(
+          name: Value(name),
+          email: Value(email),
+          passwordHash: Value(passwordHash),
+        ),
+      );
 }
