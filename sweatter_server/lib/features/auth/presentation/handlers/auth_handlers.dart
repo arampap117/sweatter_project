3af3e9b1a9444996e6ecfe45a9bf79eea1320eac
arampap117 <arampap117@gmail.com>
2026-02-07 17:14:44 +0300

    Настройка AuthHandler и AuthRouter

--- sweatter_server/lib/features/auth/presentation/handlers/auth_handlers.dart
+++ sweatter_server/lib/features/auth/presentation/handlers/auth_handlers.dart
@@ -1,1 +1,53 @@
-class AuthHandler {}
+import 'dart:convert';
+
+import 'package:shelf/shelf.dart';
+import 'package:sweatter_server/features/auth/domain/entities/user_entiti.dart';
+import 'package:sweatter_server/features/auth/domain/services/auth_service.dart';
+
+class AuthHandler {
+  const AuthHandler({required this.service});
+  final AuthService service;
+
+  Future<Response> register(final Request req) async {
+    final String reqData = await req.readAsString();
+    final Map<String, dynamic> data =
+        jsonDecode(reqData) as Map<String, dynamic>;
+
+    try {
+      await service.register(
+        email: data['email'],
+        name: data['name'],
+        password: data['password'],
+      );
+      return Response.ok(
+        jsonEncode({'message': 'User created'}),
+        headers: {'content-type': 'application/json'},
+      );
+    } catch (exception) {
+      return Response.badRequest(
+        body: jsonEncode({'error': exception.toString()}),
+        headers: {'content-type': 'application/json'},
+      );
+    }
+  }
+
+  Future<Response> login(final Request req) async {
+    final String reqData = await req.readAsString();
+    final Map<String, dynamic> data =
+        jsonDecode(reqData) as Map<String, dynamic>;
+
+    try {
+      final User user = await service.login(
+        email: data['email'],
+        password: data[' password'],
+      );
+      return Response.ok(
+        jsonEncode({'id': user.id, 'name': user.name, 'email': user.email}),
+      );
+    } catch (exception) {
+      return Response.unauthorized(
+        jsonEncode({'message': exception.toString()}),
+      );
+    }
+  }
+}
--- sweatter_server/lib/features/auth/presentation/routes/auth_router.dart
+++ sweatter_server/lib/features/auth/presentation/routes/auth_router.dart
@@ -1,11 +1,14 @@
 import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
+import 'package:sweatter_server/features/auth/presentation/handlers/auth_handlers.dart';
 
 final class AuthRouter {
-  static Handler get create =>
+  const AuthRouter({required this.handler});
+  final AuthHandler handler;
+
+  Handler get create =>
       (Router()
-            ..post('/login', () {})
-            ..post('/register', () {})
-            ..get('/me', () {}))
+            ..post('/login', handler.login)
+            ..post('/register', handler.register))
           .call;
 }
